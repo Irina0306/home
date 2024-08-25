f6c91d45ab7a7e8d71031aa0b10411a19cf95051
Irina0306 <168962012+Irina0306@users.noreply.github.com>
2024-08-25 14:58:25 +0300

    module_7_2.py

---
+++ module7/module_7_2.py
@@ -0,0 +1,31 @@
+from pprint import pprint
+import io
+from typing import List, Tuple, TextIO, Any
+
+
+def custom_write(file_name, strings):
+
+    strings_position = {}
+    file = open(file_name, 'w', encoding='UTF-8')
+    #print(file.tell())
+    #for i in strings:
+        #strings_position[(strings.index(i) + 1, file.tell())] = i
+        #file.write(f'{i}\n')
+    for i, string in enumerate(strings):
+        strings_position[(i + 1, file.tell())] = string
+        file.write(f'{string}\n')
+    return strings_position
+    file.close()
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.'
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
+
